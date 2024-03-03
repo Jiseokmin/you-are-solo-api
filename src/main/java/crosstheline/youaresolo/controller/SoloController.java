6dc563e50e9957595c0f9437e984eb86d82de05f
jiseokmin <37jr@naver.com>
2024-03-03 15:35:22 +0900

    Feat: add filter api

--- src/main/java/crosstheline/youaresolo/controller/SoloController.java
+++ src/main/java/crosstheline/youaresolo/controller/SoloController.java
@@ -1,20 +1,17 @@
 package crosstheline.youaresolo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WolfController {
-    @GetMapping("/wolf-sounds")
-    fun getWolf(sound: String): ResponseEntity<Any> {
-        val wolf = when {
-            sound.contains("아우우") -> "하얀 늑대"
-            sound.contains("하우우") -> "파란 늑대"
-            sound.contains("와우우") -> "검은 늑대"
-            sound.contains("캬우우") -> "빨간 늑대"
-            else -> "회색 늑대"
+public class SoloController {
+    @GetMapping("/filter")
+    public String filter(@RequestParam String words) {
+        // 입력된 단어 중에 욕이 포함될 경우 반응 리턴
+        if (words.contains("닥쳐")) {
+            return "어떻게 나한테 그런말을 할 수 가 있어?? 넌 정말 0점짜리 사람이구나";
         }
-
-        return ResponseEntity.ok(wolf)
+        return "";
     }
 }
